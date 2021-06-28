13617d8f5f2203142829a769666850ae62cf05d6
Fringido <mmaaxx77@msn.com>
2021-06-28 11:29:32 -0500

    Creacion de datos java y python

--- JAVA/main.java
+++ JAVA/main.java
@@ -5,5 +5,17 @@ class Main
     public static void main(String[] args) 
     {
         System.out.println("hola mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "andres herera";
+        car.passengers = 4;
+
+        System.out.println("car license: " + car.license);
+
+        Car car2 = new Car();
+        car2.license = "AMQ564";
+        car2.driver = "andreas herera";
+        car2.passengers = 3;
+        System.out.println("car license: " + car2.license);
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,7 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola Mundo")
+    car = Car()
+    car.license = "AMS335"
+    print(vars(car))
